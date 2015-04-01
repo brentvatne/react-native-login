f35d8a735855a6e2dde6266f292c79df81730280
Brent Vatne <brent.vatne@madriska.com>
2015-03-31 23:47:23 -0700

    Add a modal

--- index.ios.js
+++ index.ios.js
@@ -7,12 +7,80 @@ var {
   Text,
   View,
   TouchableHighlight,
-  Image
+  TouchableOpacity,
+  Image,
 } = React;
 
 var FacebookLoginManager = require('NativeModules').FacebookLoginManager;
 var Video = require('react-native-video');
 var LinearGradient = require('react-native-linear-gradient');
+
+var Modal = React.createClass({
+  render() {
+    if (this.props.isVisible) {
+      return (
+        <View style={modalStyles.container}>
+          <View style={modalStyles.backdrop} />
+          <View style={modalStyles.closeButton}>
+            <TouchableOpacity onPress={this.props.onClose}>
+              <Text style={modalStyles.closeButtonText}>Close</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={modalStyles.modal}>
+            {this.props.children}
+          </View>
+        </View>
+      );
+    } else {
+      return <View />;
+    }
+  },
+});
+
+var modalStyles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  backdrop: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#000000',
+    opacity: 0.5,
+  },
+  closeButton: {
+    position: 'absolute',
+    borderColor: '#ffffff',
+    borderRadius: 2,
+    borderWidth: 1,
+    right: 20,
+    top: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  closeButtonText: {
+    color: '#ffffff',
+  },
+  modal: {
+    margin: 20,
+    backgroundColor: '#ffffff',
+    borderRadius: 3,
+    padding: 20,
+    alignSelf: 'stretch',
+  }
+});
 
 var FacebookLogin = React.createClass({
   getInitialState() {
@@ -48,6 +116,14 @@ var FacebookLogin = React.createClass({
     }
   },
 
+  openModal() {
+    this.setState({modalIsOpen: true});
+  },
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  },
+
   renderLogInView() {
     return (
       <View style={styles.container}>
@@ -60,11 +136,25 @@ var FacebookLogin = React.createClass({
             </Text>
           </LinearGradient>
         </TouchableHighlight>
-        <TouchableHighlight>
-          <Text style={styles.result}>
-            {this.state.result}
+
+        <View style={styles.footer}>
+          <TouchableOpacity onPress={this.openModal} style={styles.aboutButton}>
+            <Text style={styles.aboutButtonText}>
+              About this project
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <Modal isVisible={this.state.modalIsOpen} onClose={this.closeModal}>
+          <Text style={styles.aboutTitle}>
+            Welcome to the about section!
           </Text>
-        </TouchableHighlight>
+
+          <Text style={styles.aboutBody}>
+            This is just for fun to use a modal, nothing fancy to see here. A big thanks
+            the react-native team for this wonderful tool!
+          </Text>
+        </Modal>
       </View>
     );
   },
@@ -141,6 +231,14 @@ var styles = StyleSheet.create({
     margin: 10,
     color: '#ffffff',
   },
+  aboutButtonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#efefef',
+    opacity: 0.8,
+  },
   contentContainer: {
     position: 'absolute',
     flex: 1,
@@ -166,6 +264,22 @@ var styles = StyleSheet.create({
     marginTop: 15,
     alignSelf: 'center',
   },
+  footer: {
+    position: 'absolute',
+    bottom: 10,
+    backgroundColor: 'transparent',
+    left: 0,
+    right: 0,
+  },
+  aboutButton: {
+  },
+  aboutTitle: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  aboutBody: {
+
+  }
 });
 
 AppRegistry.registerComponent('FacebookLogin', () => FacebookLogin);
