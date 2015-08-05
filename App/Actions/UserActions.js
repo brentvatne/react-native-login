c7799f1a3c9e5bc2636172e8eea4e5ffc91f723b
David Kaminsky <dkaminsky@ifwe.co>
2015-08-05 13:32:01 -0700

    Initial commit to try to upgrade to React Native 0.8.

--- App/Actions/UserActions.js
+++ App/Actions/UserActions.js
@@ -1,15 +1,20 @@
+var React = require('react-native');
+var {
+  AlertIOS,
+} = React;
+
 var FacebookLoginManager = require('NativeModules').FacebookLoginManager;
 var dispatcher = require('../AppDispatcher');
 var UserStore = require('../Stores/UserStore');
 var UserConstants = require('../Constants/UserConstants');
 var FacebookApi = require('../Apis/FacebookApi');
-var AlertIOS = require('react-native').AlertIOS;
 
 module.exports = {
   newFacebookSession() {
     FacebookLoginManager.newSession((error, info) => {
+      alert('test');
       if (error) {
-        AlertIOS.alert('Unable to sign in');
+        //AlertIOS.alert('Unable to sign in');
       } else {
         FacebookApi.getUserInfo(info.userId, info.token);
       }
